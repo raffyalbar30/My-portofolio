90ee76f2f8172636ecadc9feecd81c9ce29997a1
raffyalbar30 <geminglol09@gmail.com>
2024-06-22 11:10:54 +0700

    "update: skilss tooltip"

--- src/Component/UI/Skills.jsx
+++ src/Component/UI/Skills.jsx
@@ -3,81 +3,64 @@ import React from 'react';
 const Skills = () => {
     return (
         <>
-       <div className='flex justify-center laptop:top-0'>
+       <div className='flex justify-center laptop:top-0 z-10 relative'>
         <div className='cursor-pointer'>
          <p className='text-white font-bold hover:scale-[1.02] text-center text-4xl pb-2'>Skills⚒️</p>
          <div className="md:w-32 w-full md:h-[6px] h-1 bg-gradient-to-l from-purple-400 via-purple-300 to-purple-500 -translate-x-1 rounded-full"></div>
          <div className="md:w-32 w-full md:h-[6px] h-1 bg-purple-200 rounded-full translate-x-1"></div>
         </div>
           </div>
-            <div className='flex justify-center  laptop:pt-16 pt-8 cursor-pointer'>
-               <div className='grid grid-cols-4 laptop:grid-cols-6 tablet:grid-cols-4 laptop:gap-x-12 gap-x-4 gap-4 laptop:gap-y-8'>
-                  <div className='p-4 hover:border transition-all duration-300 hover:scale-110 border-purple-500  rounded-md  '>
-                       <img src="/public/Icons/html.webp" alt="" className='laptop:w-[100px] tablet:w-[100px] w-8 relative z-0 rounded-lg transition-all duration-300 hover:scale-110'  />
-                       <div class="opacity-0 hover:opacity-100 duration-300 absolute inset-0 z-10 laptop:pt-36 pt-16 flex justify-center items-center laptop:text-xl text-sm text-white font-semibold "> <span className='border rounded-md laptop:rounded-none laptop:px-8 laptop:mt-2 px-2 border-purple-500'>Html</span></div>
-                  </div>
-                  <div className='p-4  hover:border transition-all duration-300 hover:scale-110 border-purple-500  rounded-md'>
-                       <img src="/public/Icons/css.webp" alt="" className='laptop:w-[100px] tablet:w-[100px] w-8 relative z-0 rounded-lg transition-all duration-300 hover:scale-110'  />
-                       <div class="opacity-0 hover:opacity-100 duration-300 absolute inset-0 z-10 laptop:pt-36 pt-16 flex justify-center items-center laptop:text-xl text-sm text-white font-semibold "> <span className='border rounded-md laptop:rounded-none laptop:px-8 laptop:mt-2 px-2 border-purple-500'>Css</span></div>
-                  </div>
-                  <div className='p-4  hover:border transition-all duration-300 hover:scale-110 border-purple-500  rounded-md'>
-                       <img src="/public/Icons/javascript.webp" alt="" className='laptop:w-[100px] tablet:w-[100px] w-8 relative z-0 rounded-lg transition-all duration-300 hover:scale-110'  />
-                       <div class="opacity-0 hover:opacity-100 duration-300 absolute inset-0 z-10 laptop:pt-36 pt-16 flex justify-center items-center laptop:text-xl text-sm text-white font-semibold "> <span className='border rounded-md laptop:rounded-none laptop:px-8 laptop:mt-2 px-2 border-purple-500'>Javascript</span></div>
-                  </div>
-                  <div className='p-4  hover:border transition-all duration-300 hover:scale-110 border-purple-500  rounded-md'>
-                       <img src="/public/Icons/git.webp" alt="" className='laptop:w-[100px] tablet:w-[100px] w-8 relative z-0 rounded-lg transition-all duration-300 hover:scale-110'  />
-                       <div class="opacity-0 hover:opacity-100 duration-300 absolute inset-0 z-10 laptop:pt-36 pt-16 flex justify-center items-center laptop:text-xl text-sm text-white font-semibold "> <span className='border rounded-md laptop:rounded-none laptop:px-8 laptop:mt-2 px-2 border-purple-500'>Git</span></div>
-                  </div>
-                  <div className='p-4  hover:border transition-all duration-300 hover:scale-110 border-purple-500  rounded-md'>
-                       <img src="/public/Icons/github.webp" alt="" className='laptop:w-[100px] tablet:w-[100px] w-8 relative z-0 rounded-lg transition-all duration-300 hover:scale-110'  />
-                       <div class="opacity-0 hover:opacity-100 duration-300 absolute inset-0 z-10 laptop:pt-36 pt-16 flex justify-center items-center laptop:text-xl text-sm text-white font-semibold "> <span className='border rounded-md laptop:rounded-none laptop:px-8 laptop:mt-2 px-2 border-purple-500'>Github</span></div>
-                  </div>
-                  <div className='p-4  hover:border transition-all duration-300 hover:scale-110 border-purple-500  rounded-md'>
-                       <img src="/public/Icons/bootstrap.webp" alt="" className='laptop:w-[100px] tablet:w-[100px] w-8 relative z-0 rounded-lg transition-all duration-300 hover:scale-110'  />
-                       <div class="opacity-0 hover:opacity-100 duration-300 absolute inset-0 z-10 laptop:pt-36 pt-16 flex justify-center items-center laptop:text-xl text-sm text-white font-semibold "> <span className='border rounded-md laptop:rounded-none laptop:px-8 laptop:mt-2 px-2 border-purple-500'>Bootstrap</span></div>
-                  </div>
-                  <div className='p-4  hover:border transition-all duration-300 hover:scale-110 border-purple-500  rounded-md'>
-                       <img src="/public/Icons/tailwind.webp" alt="" className='laptop:w-[100px]  tablet:w-[100px] w-8 relative z-0 rounded-lg transition-all duration-300 hover:scale-110'  />
-                       <div class="opacity-0 hover:opacity-100 duration-300 absolute inset-0 z-10 laptop:pt-36 pt-16 flex justify-center items-center laptop:text-xl text-sm text-white font-semibold "> <span className='border rounded-md laptop:rounded-none laptop:px-8 laptop:mt-2 px-2 border-purple-500'>Tailwindcss</span></div>
-                  </div>
-                  <div className='p-4  hover:border transition-all duration-300 hover:scale-110 border-purple-500  rounded-md'>
-                       <img src="/public/Icons/sass.svg" alt="" className='laptop:w-[100px] tablet:w-[100px] w-8 relative z-0 rounded-lg transition-all duration-300 hover:scale-110'  />
-                       <div class="opacity-0 hover:opacity-100 duration-300 absolute inset-0 z-10 laptop:pt-36 pt-16 flex justify-center items-center laptop:text-xl text-sm text-white font-semibold "> <span className='border rounded-md laptop:rounded-none laptop:px-8 laptop:mt-2 px-2 border-purple-500'>Sass</span></div>
-                  </div>
-                  <div className='p-4  hover:border transition-all duration-300 hover:scale-110 border-purple-500  rounded-md'>
-                       <img src="/public/Icons/react.webp" alt="" className='laptop:w-[100px] tablet:w-[100px] w-8 relative z-0 rounded-lg transition-all duration-300 hover:scale-110'  />
-                       <div class="opacity-0 hover:opacity-100 duration-300 absolute inset-0 z-10 laptop:pt-36 pt-16 flex justify-center items-center laptop:text-xl text-sm text-white font-semibold "> <span className='border rounded-md laptop:rounded-none laptop:px-8 laptop:mt-2 px-2 border-purple-500'>ReactJs</span></div>
-                  </div>
-                  <div className='p-4  hover:border transition-all duration-300 hover:scale-110 border-purple-500  rounded-md'>
-                       <img src="/public/Icons/nextjs.svg" alt="" className='laptop:w-[100px] tablet:w-[100px] w-8 relative z-0 rounded-lg transition-all duration-300 hover:scale-110'  />
-                       <div class="opacity-0 hover:opacity-100 duration-300 absolute inset-0 z-10 laptop:pt-36 pt-16 flex justify-center items-center laptop:text-xl text-sm text-white font-semibold "> <span className='border rounded-md laptop:rounded-none laptop:px-8 laptop:mt-2 px-2 border-purple-500'>Next Js</span></div>
-                  </div>
-                  <div className='p-4  hover:border transition-all duration-300 hover:scale-110 border-purple-500  rounded-md'>
-                       <img src="/public/Icons/redux.svg" alt="" className='laptop:w-[100px] tablet:w-[100px] w-8 relative z-0 rounded-lg transition-all duration-300 hover:scale-110'  />
-                       <div class="opacity-0 hover:opacity-100 duration-300 absolute inset-0 z-10 laptop:pt-36 pt-16 flex justify-center items-center laptop:text-xl text-sm text-white font-semibold "> <span className='border rounded-md laptop:rounded-none laptop:px-8 laptop:mt-2 px-2 border-purple-500'>Redux</span></div>
-                  </div>
-                  <div className='p-4  hover:border transition-all duration-300 hover:scale-110 border-purple-500  rounded-md'>
-                       <img src="/public/Icons/node-js.svg" alt="" className='laptop:w-[100px] tablet:w-[100px] w-8 relative z-0 rounded-lg transition-all duration-300 hover:scale-110'  />
-                       <div class="opacity-0 hover:opacity-100 duration-300 absolute inset-0 z-10 laptop:pt-36 pt-16 flex justify-center items-center laptop:text-xl text-sm text-white font-semibold "> <span className='border rounded-md laptop:rounded-none laptop:px-8 laptop:mt-2 px-2 border-purple-500'>Nodejs</span></div>
-                  </div>
-                  <div className='p-4  hover:border transition-all duration-300 hover:scale-110 border-purple-500  rounded-md'>
-                       <img src="/public/Icons/mysql.webp" alt="" className='laptop:w-[100px] tablet:w-[100px] w-8 relative z-0 rounded-lg transition-all duration-300 hover:scale-110'  />
-                       <div class="opacity-0 hover:opacity-100 duration-300 absolute inset-0 z-10 laptop:pt-36 pt-16 flex justify-center items-center laptop:text-xl text-sm text-white font-semibold "> <span className='border rounded-md laptop:rounded-none laptop:px-8 laptop:mt-2 px-2 border-purple-500'>Mysql</span></div>
-                  </div>
-                  <div className='p-4  hover:border transition-all duration-300 hover:scale-110 border-purple-500  rounded-md'>
-                       <img src="/public/Icons/mongodb.svg" alt="" className='laptop:w-[100px] tablet:w-[100px] w-8 relative z-0 rounded-lg transition-all duration-300 hover:scale-110'  />
-                       <div class="opacity-0 hover:opacity-100 duration-300 absolute inset-0 z-10 laptop:pt-36 pt-16 flex justify-center items-center laptop:text-xl text-sm text-white font-semibold "> <span className='border rounded-md laptop:rounded-none laptop:px-8 laptop:mt-2 px-2 border-purple-500'>MongoDB</span></div>
-                  </div>
-                  <div className='p-4  hover:border transition-all duration-300 hover:scale-110 border-purple-500  rounded-md'>
-                       <img src="/public/Icons/linux.png" alt="" className='laptop:w-[100px] tablet:w-[100px] w-8 relative z-0 rounded-lg transition-all duration-300 hover:scale-110'  />
-                       <div class="opacity-0 hover:opacity-100 duration-300 absolute inset-0 z-10 laptop:pt-36 pt-16 flex justify-center items-center laptop:text-xl text-sm text-white font-semibold "> <span className='border rounded-md laptop:rounded-none laptop:px-8 laptop:mt-2 px-2 border-purple-500'>Linux</span></div>
-                  </div>
-                  <div className='p-4  hover:border transition-all duration-300 hover:scale-110 border-purple-500  rounded-md'>
-                       <img src="/public/Icons/figma.svg" alt="" className='laptop:w-[100px] tablet:w-[100px] w-8 relative z-0 rounded-lg transition-all duration-300 hover:scale-110'  />
-                       <div class="opacity-0 hover:opacity-100 duration-300 absolute inset-0 z-10 laptop:pt-36 pt-16 flex justify-center items-center laptop:text-xl text-sm text-white font-semibold "> <span className='border rounded-md laptop:rounded-none laptop:px-8 laptop:mt-2 px-2 border-purple-500'>Figma</span></div>
-                  </div>
-               </div>
+          <div className='flex justify-center '>
+            <div className='laptop:grid laptop:ml-0 ml-12 mt-12 laptop:grid-cols-5 gap-x-8 '>
+          <div data-tooltip-content="Tailwind CSS" class="icon-tooltip transition-colors duration-200 group inline-block rounded-md p-3.5 hover:bg-indigo-200/30 dark:hover:bg-zinc-500/30">
+              <img src="/public/icons/html.webp" alt="" className=' laptop:w-[100px] w-[50px] ' />
+          </div>
+          <div data-tooltip-content="Tailwind CSS" class="icon-tooltip transition-colors duration-200 group inline-block rounded-md p-3.5 hover:bg-indigo-200/30 dark:hover:bg-zinc-500/30">
+              <img src="/public/icons/css.webp" alt="" className=' laptop:w-[100px] w-[50px] ' />
+          </div>
+          <div data-tooltip-content="Tailwind CSS" class="icon-tooltip transition-colors duration-200 group inline-block rounded-md p-3.5 hover:bg-indigo-200/30 dark:hover:bg-zinc-500/30">
+              <img src="/public/icons/javascript.webp" alt="" className=' laptop:w-[100px] w-[50px] ' />
+          </div>
+          <div data-tooltip-content="Tailwind CSS" class="icon-tooltip transition-colors duration-200 group inline-block rounded-md p-3.5 hover:bg-indigo-200/30 dark:hover:bg-zinc-500/30">
+              <img src="/public/icons/tailwind.webp" alt="" className=' laptop:w-[100px] w-[50px] ' />
+          </div>
+          <div data-tooltip-content="Tailwind CSS" class="icon-tooltip transition-colors duration-200 group inline-block rounded-md p-3.5 hover:bg-indigo-200/30 dark:hover:bg-zinc-500/30">
+              <img src="/public/icons/bootstrap.webp" alt="" className=' laptop:w-[100px] w-[50px] ' />
+          </div>
+          
+          <div data-tooltip-content="Tailwind CSS" class="icon-tooltip transition-colors laptop:mt-12 duration-200 group inline-block rounded-md p-3.5 hover:bg-indigo-200/30 dark:hover:bg-zinc-500/30">
+              <img src="/public/icons/react.webp" alt="" className=' laptop:w-[100px] w-[50px] ' />
+          </div>
+          <div data-tooltip-content="Tailwind CSS" class="icon-tooltip transition-colors laptop:mt-12 duration-200 group inline-block rounded-md p-3.5 hover:bg-indigo-200/30 dark:hover:bg-zinc-500/30">
+              <img src="/public/icons/nextjs.svg" alt="" className=' laptop:w-[100px] w-[50px] ' />
+          </div>
+          <div data-tooltip-content="Tailwind CSS" class="icon-tooltip transition-colors laptop:mt-12 duration-200 group inline-block rounded-md p-3.5 hover:bg-indigo-200/30 dark:hover:bg-zinc-500/30">
+              <img src="/public/icons/redux.svg" alt="" className=' laptop:w-[100px] w-[50px] ' />
+          </div>
+          
+          <div data-tooltip-content="Tailwind CSS" class="icon-tooltip transition-colors laptop:mt-12 duration-200 group inline-block rounded-md p-3.5 hover:bg-indigo-200/30 dark:hover:bg-zinc-500/30">
+              <img src="/public/icons/sass.svg" alt="" className=' laptop:w-[100px] w-[50px] ' />
+          </div>
+          <div data-tooltip-content="Tailwind CSS" class="icon-tooltip transition-colors laptop:mt-12 duration-200 group inline-block rounded-md p-3.5 hover:bg-indigo-200/30 dark:hover:bg-zinc-500/30">
+              <img src="/public/icons/node-js.svg" alt="" className=' laptop:w-[100px] w-[50px] ' />
+          </div>
+          <div data-tooltip-content="Tailwind CSS" class="icon-tooltip transition-colors laptop:mt-12 duration-200 group inline-block rounded-md p-3.5 hover:bg-indigo-200/30 dark:hover:bg-zinc-500/30">
+              <img src="/public/icons/mysql.webp" alt="" className=' laptop:w-[100px] w-[50px] ' />
+          </div>
+          <div data-tooltip-content="Tailwind CSS" class="icon-tooltip transition-colors laptop:mt-12 duration-200 group inline-block rounded-md p-3.5 hover:bg-indigo-200/30 dark:hover:bg-zinc-500/30">
+              <img src="/public/icons/mongodb.svg" alt="" className=' laptop:w-[100px] w-[50px]' />
+          </div>
+          <div data-tooltip-content="Tailwind CSS" class="icon-tooltip transition-colors laptop:mt-12 duration-200 group inline-block rounded-md p-3.5 hover:bg-indigo-200/30 dark:hover:bg-zinc-500/30">
+              <img src="/public/icons/git.webp" alt="" className=' laptop:w-[100px] w-[50px] ' />
+          </div>
+          <div data-tooltip-content="Tailwind CSS" class="icon-tooltip transition-colors laptop:mt-12 duration-200 group inline-block rounded-md p-3.5 hover:bg-indigo-200/30 dark:hover:bg-zinc-500/30">
+              <img src="/public/icons/figma.svg" alt="" className=' laptop:w-[100px] w-[50px] ' />
+          </div>
+          <div data-tooltip-content="Tailwind CSS" class="icon-tooltip transition-colors laptop:mt-12 duration-200 group inline-block rounded-md p-3.5 hover:bg-indigo-200/30 dark:hover:bg-zinc-500/30">
+              <img src="/public/icons/linux.png" alt="" className=' laptop:w-[100px] w-[50px] ' />
+          </div>
             </div>
+          </div>
         </>
     );  
 }
--- src/pages/View_detail.jsx
+++ src/pages/View_detail.jsx
@@ -2,6 +2,13 @@ import React from 'react';
 import { FaCalendarAlt } from "react-icons/fa";
 import ContainerCards from '../Component/utilitis/Container-card';
 import Navbar from '../Component/Navbar/Navbar';
+import { TbWorld } from "react-icons/tb";
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaArrowLeftLong } from "react-icons/fa6";
+
+
+
 
 const ViewDetail = () => {
     return (
@@ -10,7 +17,10 @@ const ViewDetail = () => {
         <div className='w-full backdrop-blur-lg bg-slate-100 opacity-30 h-24'> </div>
         <div className=' w-full absolute bg-zinc-800 rounded-t-lg z-10'>
              <div className='ml-12 mr-12'>
-               <p className='text-xl text-purple-500 pt-12'>Back to home page</p>
+               <div className='flex items-center gap-x-4 cursor-pointer group'>
+               <span className='mt-12 text-2xl group-hover:scale-110'> <FaArrowLeftLong /></span>
+               <p className='text-xl text-purple-500 pt-12 group-hover:hover:underline underline-offset-8'>Back to home page</p>
+               </div>
                <p className='text-4xl font-sans font-semibold pt-8'>Coffe shop UMKM Cilamaya</p>
                <p className='text-2xl font-sans font-semibold pt-4'>E-commerce page use Rect js Tailwindcss Redux and another library</p>
                <p className='pt-8 flex gap-x-2'><span className='text-xl'><FaCalendarAlt /></span>Januari 06 2024 - Maret 28 2024</p>
@@ -29,28 +39,28 @@ const ViewDetail = () => {
              </div>
              <div className='mt-8'>
                 <p className='font-sans font-semibold text-2xl'>Tech stack</p>
-                <div className='flex justify-center laptop:justify-normal gap-x-4 pt-4'>
-                                  <img src="/public/Icons/html.webp" alt="" className='w-[50px]' />
-                                  <img src="/public/Icons/css.webp" alt="" className='w-[50px]' />
-                                  <img src="/public/Icons/javascript.webp" alt="" className='w-[50px]' />
-                                  <img src="/public/Icons/sass.svg" alt="" className='w-[50px]' />
-                                  <img src="/public/Icons/bootstrap.webp" alt="" className='w-[50px]' />
-                                  </div>
+              
+                                  <img src="/public/Icons/sass.svg" alt="" className='w-[50px] mt-4' />
+                                  <img src="/public/Icons/bootstrap.webp" alt="" className='w-[50px] mt-4' />
+                    
              </div>
               <div className='laptop:flex laptop:justify-between'>
              <div className='mt-8'>
                 <p className='font-sans font-semibold text-2xl'>Descriptions</p>
                 <p className='font-sans text-gray-400 laptop:text-left text-justify'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt, aperiam hic, ea rerum vitae assumenda debitis earum aspernatur corporis officia omnis cumque possimus totam illo, accusantium ex quod voluptates quaerat. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi adipisci quisquam deleniti dolores optio quis numquam architecto similique, placeat iste ex autem minus aliquam eaque animi id illum itaque quae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste consectetur natus perspiciatis error, pariatur voluptates aut?</p>
              </div>
-               <div className='mr-2 border p-8 rounded-md mt-4'>
-                    <div className='text-center mx-auto border w-48 py-2 rounded-md'>
+               <div className='laptop:mr-2 border border-zinc-600 laptop:p-8 w-auto p-4 rounded-md mt-4'>
+                    <div className='text-center mx-auto border w-48 flex items-center gap-x-4 py-2 rounded-md'>
+                       <span className='ml-2 text-xl '><TbWorld /></span>
                       <a href="" className='text-sm'>Visite online site</a>
                     </div>
-                    <div className='text-center mx-auto border w-48 py-2 mt-2 rounded-md'>
-                      <a href="" className='text-sm'>Visite github repository</a>
+                    <div className='text-center mx-auto border mt-2 w-48 flex items-center gap-x-4 py-2 rounded-md'>
+                       <span className='ml-2 text-xl'><FaGithub /></span>
+                      <a href="" className='text-sm'>github Repository</a> 
                     </div>
-                    <div className='text-center mx-auto border w-48 py-2 mt-2 rounded-md'>
-                      <a href="" className='text-sm'>follow my repository</a>
+                    <div className='text-center mx-auto border mt-2 w-48 flex items-center gap-x-4 py-2 rounded-md'>
+                       <span className='ml-2 text-xl'><FaLinkedin /></span>
+                      <a href="" className='text-sm'>follow me in linkedln</a>
                     </div>
                </div>
               </div>
