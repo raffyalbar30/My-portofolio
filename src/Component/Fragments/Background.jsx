ab1c20667a8c311f7c45233c542a669f081847d6
raffyalbar30 <geminglol09@gmail.com>
2024-06-27 14:23:59 +0700

    update: certifity

--- src/Component/Fragments/Background.jsx
+++ src/Component/Fragments/Background.jsx
@@ -8,14 +8,15 @@ const Background = () => {
   return (
     <>
       <div className="w-full absolute z-10">
+        <div className="w-full  bg-black backdrop-blur-sm opacity-75 h-24 absolute z-20"></div>
         <img
           src="/public/img/dark-layered-wave.svg"
           alt=""
-          className="rotate-180 laptop:mt-4 mt-8 opacity-70"
+          className="rotate-180 laptop:mt-4 mt-8"
         />
       </div>
       <div className="absolute laptop:mt-8 mt-10">
-        <div className="bg-boxes-light bg-[length:20px_20px] dark:bg-boxes-dark  laptop:pt-40 tablet:pt-28 pt-12 ">
+        <div className="bg-boxes-light bg-[length:20px_20px] dark:bg-boxes-dark bg-black laptop:pt-40 tablet:pt-28 pt-12 ">
           <div className="relative laptop:pb-52 pb-24 z-20 ">
             <Skills></Skills>
             <Projects></Projects>
--- src/Component/UI/Certificates.jsx
+++ src/Component/UI/Certificates.jsx
@@ -1,75 +1,92 @@
-import React from 'react';
+
 import ContainerCards from '../utilitis/Container-card';
 import { useState } from 'react';
 
-const Certificates = () => {
-    const [isChange, setisChange] = useState();
+const datas = [
+   {
+      id_data : 1,
+      label   : "international certify",
+      role    : "python" ,
+      Image   : "/public/Certify/pythoncertify.jpeg"
+
+   },
+   { 
+       id_data : 2,
+       label : "national certify", 
+       role    : "javascript" ,
+       Image  : "/public/Certify/flutercertify.jpeg"
+
+   },
+   {
+       id_data : 3,
+       label   : "workshop certify",
+       role    : "c++" , 
+       Image   : "/public/Certify/pythoncertify.jpeg"
+
+    },
+    { 
+        id_data : 4,
+        label : "chanel certify", 
+        role    : "java" ,
+        Image  : "/public/Certify/flutercertify.jpeg"
+
+    }
+];
+
+  const Certificates = () => {
+
+     const items = datas;
+     const [selected, setselected] = useState(items[0])
+
+  
     return (
         <>
         <div className='border laptop:w-[1200px] h-auto rounded-sm p-2 border-zinc-600'>
        <div className='p-2'>
           <div>
            <p className='text-white/60 mt-4 text-xl'>Reacent Update Certifity</p>
+           {
            <div className='flex justify-between'>
             <div className='mt-4 mr-2 mb-2'>
             <ul className='mt-4'>
-            <li class="relative flex items-baseline gap-6 pb-5">
-            <div class="before:absolute before:left-[5.5px] before:h-full before:w-[1px] before:bg-gray-400">
-            <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" class="bi bi-circle-fill fill-gray-400" viewBox="0 0 16 16">
-                <circle cx="8" cy="8" r="8" />
-            </svg>
-            </div>
-            <div>
-            <p class="text-sm text-gray-400">18-8-2022</p>
-            <p class="text-sm text-gray-400">Python Certifity</p>
-            <p class="text-sm text-gray-400">International Certifity</p>
-            <p class="mt-2 text-gray-400 text-sm">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores incidunt blanditiis dignissimos, enim earum mollitia.</p>
-            </div>
-            </li>
+               {
+                  items.map( item => (
+                   <li class="relative flex items-baseline gap-6 pb-5" key={item.id_data}>
+                    <div class="before:absolute before:left-[5.5px] before:h-full before:w-[1px] before:bg-gray-400">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" class="bi bi-circle-fill fill-gray-400" viewBox="0 0 16 16">
+                        <circle cx="8" cy="8" r="8" />
+                    </svg>
+                    </div>
+                    <div>
+                    <p className="text-sm text-gray-400">18-8-2022</p>
+                    <p className={` text-sm text-gray-400 cursor-pointer`} onClick={()=> setselected(item)}>{item.label}</p>
+                    <p className="mt-2 text-gray-400 text-sm">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores incidunt blanditiis dignissimos, enim earum mollitia.</p>
+                    </div>
+                        </li>
 
-            <li class="relative flex items-baseline gap-6 pb-5">
-            <div class="before:absolute before:left-[5.5px] before:h-full before:w-[1px] before:bg-gray-400">
-            <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" class="bi bi-circle-fill fill-gray-400" viewBox="0 0 16 16">
-                <circle cx="8" cy="8" r="8" />
-            </svg>
-            </div>
-            <div>
-            <p class="text-sm text-gray-400">18-8-2022</p>
-            <p class="text-sm text-gray-400">Python Certifity</p>
-            <p class="text-sm text-gray-400">International Certifity</p>
-            <p class="mt-2 text-gray-400 text-sm">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores incidunt blanditiis dignissimos, enim earum mollitia.</p>
-            </div>
-            </li>
+                  ))
 
-            <li class="relative flex items-baseline gap-6 pb-5">
-            <div class="before:absolute before:left-[5.5px]">
-            <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" class="bi bi-circle-fill fill-gray-400" viewBox="0 0 16 16">
-                <circle cx="8" cy="8" r="8" />
-            </svg>
-            </div>
-            <div>
-            <p class="text-sm text-gray-400">18-8-2022</p>
-            <p class="text-sm text-gray-400">Python Certifity</p>
-            <p class="text-sm text-gray-400">International Certifity</p>
-            <p class="mt-2 text-gray-400 text-sm">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores incidunt blanditiis dignissimos, enim earum mollitia.</p>
-            </div>
-            </li>
+               }
             </ul>
             </div>
             <div className='divide-y divide-dashed md:divide-solid border border-zinc-600 mb-2'></div>
-            <div className=' w-full mb-2 mr-2 ml-2'> 
-                 <div>
-                 <ContainerCards>
-                    <img
-                            style={{
-                                transform: "translateZ(50px)",
-                                transformStyle: "preserve-3d",
-                            }}
-                            src="/public/Certify/pythoncertify.jpeg" alt="" className='w-full h-[400px]' />
-               </ContainerCards>
-                 </div>
+            <div className=' w-full mb-2 mr-2 ml-2 mt-8'> 
+                 {
+                    <div key={selected.id_data}>
+                    <ContainerCards>
+                       <img
+                               style={{
+                                   transform: "translateZ(50px)",
+                                   transformStyle: "preserve-3d",
+                               }}
+                               src={selected.Image} alt="" className='w-full h-[400px]' />
+                  </ContainerCards>
+                    </div>
+                 }
+                  
             </div>
-            </div>
+           </div>
+           }
            </div>
        </div>
         </div>
